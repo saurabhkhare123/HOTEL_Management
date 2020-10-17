50602674ec01f9dfcbd8aed85c0a43b9f3ce1723
saurabhkhare123 <saurabh.khare_cs18@gla.ac.in>
2020-10-17 20:00:45 +0530

    updated

--- src/hotel/management/system/Conn.java
+++ src/hotel/management/system/Conn.java
@@ -1,7 +1,7 @@
 package hotel.management.system;
 
 import java.sql.*;
-public class conn {
+ class conn {
 
     Connection c;
     Statement s;
--- src/hotel/management/system/login.java
+++ src/hotel/management/system/login.java
@@ -7,7 +7,7 @@ import javax.swing.*;    // for JFrame , setBounds etc
 import java.awt.*;      // for Color
 import java.sql.ResultSet;
 
-public class Login extends JFrame implements ActionListener {    // ActionListener is an Interface
+class Login extends JFrame implements ActionListener {    // ActionListener is an Interface
 
     JLabel l1,l2;
     JTextField t1,t2;
@@ -15,43 +15,43 @@ public class Login extends JFrame implements ActionListener {    // ActionListen
 
     Login(){
         l1 = new JLabel("USERNAME  : ");
-        l1.setBounds(40,20,100,30); // to set Locations
+        l1.setBounds(40,30,100,30); // to set Locations
         add(l1);    // to add on the frame
         l2 = new JLabel("PASSWORD  : ");
-        l2.setBounds(40,60,100,30);
+        l2.setBounds(40,70,100,30);
         add(l2);
 
         t1 = new JTextField();     // for text , visible to the user
-        t1.setBounds(150,20,100,30);
+        t1.setBounds(150,30,150,30);
         add(t1);
         t2 = new JPasswordField(); // for passwords , invisible to user
-        t2.setBounds(150,60,100,30);
+        t2.setBounds(150,70,150,30);
         add(t2);
 
         b1 = new JButton("Login   >>> ");
-        b1.setBounds(170,140,125,30);
+        b1.setBounds(180,160,125,30);
         b1.setBackground(Color.GREEN);
         b1.setForeground(Color.WHITE);
         b1.addActionListener(this);    // for taking the action to be performed
         add(b1);
         b2 = new JButton("Cancel XXX ");
-        b2.setBounds(10,140,125,30);
+        b2.setBounds(30,160,125,30);
         b2.setBackground(Color.RED);
         b2.setForeground(Color.WHITE);
         b2.addActionListener(this);  // for taking the action
         add(b2);
 
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("hotel/management/system/icons/emp.jpg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("hotel/management/system/icons/lady.jpeg"));
         Image i2 = i1.getImage().getScaledInstance(220,220, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);                  //  for adding image to the frame
         JLabel l3 = new JLabel(i3);
-        l3.setBounds(320,5,220,220);
+        l3.setBounds(340,18,200,200);
         add(l3);
 
         getContentPane().setBackground(Color.WHITE);  // for coloring the complete frame as WHITE
 
         setLayout(null);
-        setBounds(500,300,580,250);
+        setBounds(500,300,590,280);
         setVisible(true);     // to show the Frame
 
     }
